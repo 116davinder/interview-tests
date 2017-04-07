9e3fddb41c4fbe366d624e9935372c52df8d638e
Davinder Pal Singh <davinder@xenondigilabs.com>
2017-04-07 10:38:25 +0530

    added condition for empty argument

--- assignment2/executor.sh
+++ assignment2/executor.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-# Getting default ssh user
-echo "Please enter the default user for ssh:"
-read default_user
+if [ $# -eq 0 ]; then
+    echo "No Remote Hosts provided"
+    exit 1
 
-while [ "$do_command_continue" != "no" ]
-do 
+else 
+    # Getting default ssh user
+    echo "Please enter the default user for ssh:"
+    read default_user
+
+    while [ "$do_command_continue" != "no" ]
+    do 
 	# Getting command to execute on remote hosts
 	echo "Please enter the SSH command to execute:"
 	read ssh_command
 	
 	# Executing Command on Remote Hosts
 	for HOSTNAME in "$@" ; do
+	    echo ""
+	    echo "Printing SSH command OutPut"
 	    ssh -l ${default_user} ${HOSTNAME} -p 22 "${ssh_command}"
 	done
 
 	echo "if you donot want to execute more commands ? Press 'no' "
+	echo "else Press any key to continue"
 	read do_command_continue
-done
+    done
+
+fi
