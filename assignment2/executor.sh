65caf7c358a39fe86ecf6dab66523ac96c3f4b22
Davinder Pal Singh <davinder@xenondigilabs.com>
2017-04-07 10:37:27 +0530

    added while loop for multiple commands

--- assignment2/executor.sh
+++ assignment2/executor.sh
@@ -4,11 +4,17 @@
 echo "Please enter the default user for ssh:"
 read default_user
 
-# Getting command to execute on remote hosts
-echo "Please enter the SSH command to execute:"
-read ssh_command
+while [ "$do_command_continue" != "no" ]
+do 
+	# Getting command to execute on remote hosts
+	echo "Please enter the SSH command to execute:"
+	read ssh_command
+	
+	# Executing Command on Remote Hosts
+	for HOSTNAME in "$@" ; do
+	    ssh -l ${default_user} ${HOSTNAME} -p 22 "${ssh_command}"
+	done
 
-# Executing Command on Remote Hosts
-for HOSTNAME in "$@" ; do
-    ssh -l ${default_user} ${HOSTNAME} "${ssh_command}"
+	echo "if you donot want to execute more commands ? Press 'no' "
+	read do_command_continue
 done
