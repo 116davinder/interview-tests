e01215d8e03cff21df49713a22dc13e31c0155b9
Davinder Pal <dpsangwal@gmail.com>
2018-05-23 20:57:29 +0530

    removed extra lines

--- bbm-tests/compress.sh
+++ bbm-tests/compress.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 current_date=`date '+%Y-%m-%d'`
 output_filename=$(echo $1)_$current_date.tar.bz2
 
@@ -19,31 +20,3 @@ elif [ $# -gt 0 ]
         exit 0
     fi
 fi
-#################################
-
-Let's look at a selection from the "Orders" table:
-OrderID	CustomerID	OrderDate
-10308	2	1996-09-18
-10309	37	1996-09-19
-10310	77	1996-09-20
-
-Then, look at a selection from the "Customers" table:
-CustomerID	CustomerName	ContactName	Country
-1	Alfreds Futterkiste	Maria Anders	Germany
-2	Ana Trujillo Emparedados y helados	Ana Trujillo	Mexico
-3	Antonio Moreno Taquería
-
-
-SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
-FROM Orders
-INNER JOIN Customers
-ON Orders.CustomerID=Customers.CustomerID;
-
-
-OrderID	CustomerName	OrderDate
-10308	Ana Trujillo Emparedados y helados	9/18/1996
-10365	Antonio Moreno Taquería	11/27/1996
-10383	Around the Horn	12/16/1996
-10355	Around the Horn	11/15/1996
-10278	Berglunds snabbköp	8/12/1996
-
