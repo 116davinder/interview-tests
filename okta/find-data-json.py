6fb65023312df01d981733b55ba4c138307a7c90
DAVINDER PAL <dpsangwal@gmail.com>
2025-03-14 16:05:27 +0530

    Create find-data-json.py

---
+++ okta/find-data-json.py
@@ -0,0 +1,26 @@
+1. directory a ( 100 childs )
+2. find a file with data.json
+
+import os
+
+path = "/tmp"
+
+# get all direcory
+get_all_dir_paths = os.
+
+
+def check_data_files(dir_path):
+	for file in os.get_files():
+	# if condition check if the files names matches with data.json
+        return full path
+
+for dir all_list:
+	print(check_data_files(dir))
+
+# few questions
+################
+# SSH Protocol
+# orphan and zombie process
+# terraform statefile corruption
+# encoding and encryption ( how to encrypt a file )
+# 
